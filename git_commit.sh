f6d4ae5803285b7d280f56f3e8f2966683cadb91
SerP1969 <serp@LAPTOP-96HBDEJL.>
2025-04-18 11:42:50 +0300

    Conmmit date: 2025-04-18_11:42:50

--- git_commit.sh
+++ git_commit.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 git add .
 commit_string=$(date +%F_%T)
-git commit -m "All_files: $commit_strings"
+git commit -m "Conmmit date: $commit_string"
 git branch -M main
 git remote add origin https://github.com/SerP1969/playbooks.git
 git push -u origin main
